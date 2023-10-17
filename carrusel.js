3e741cb0ccf7199c0d830ddbdf94e743687e1d3d
Ashvax <137654244+Ashvax@users.noreply.github.com>
2023-10-16 23:38:09 -0300

    carrusel

---
+++ carrusel.js
@@ -0,0 +1,55 @@
+let botonAnt= document.getElementById('left')
+let botonSig= document.getElementById('right')
+let contenedorPuntos= document.getElementById('puntitosContainer')
+let imagen = document.getElementById('imagenView')
+
+let coleccion = ["./sources/b1.jpg","./sources/b2.jpg","./sources/b3.jpg","./sources/b4.jpg"]
+
+let puntero=0;
+
+window.addEventListener('load', ()=>{
+    imagen.src= coleccion[puntero]
+    cargarPuntitos()
+    actualizarPuntos()
+})
+
+function cargarPuntitos(){
+    coleccion.forEach(()=>{
+        let p = document.createElement('div')
+        p.classList.add('punto')
+        contenedorPuntos.appendChild(p)
+    })
+}
+
+
+botonSig.addEventListener('click',()=>{
+    puntero++
+
+    if(puntero>=coleccion.length){
+        puntero=0
+    }
+    imagen.src= coleccion[puntero]
+    actualizarPuntos()
+
+})
+
+botonAnt.addEventListener('click',()=>{
+    puntero--
+    if(puntero<0){
+        puntero=coleccion.length-1
+    }
+
+    imagen.src= coleccion[puntero]
+    actualizarPuntos()
+})
+
+function actualizarPuntos(){
+    let pts= document.querySelectorAll('.punto')
+    
+    pts.forEach(p=>{
+        p.classList.remove('puntoActual')
+    })
+
+    pts[puntero].classList.add('puntoActual')
+
+}

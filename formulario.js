150d9cbf5c2a67dadc1355645e99c6653fa7827b
Ashvax <137654244+Ashvax@users.noreply.github.com>
2023-10-17 15:39:32 -0300

    contact

--- formulario.js
+++ formulario.js
@@ -1,19 +1,48 @@
-<script>
-// Función para manejar el envío del formulario
-document.getElementById('contact-form').addEventListener('submit', function (event) {
-    event.preventDefault(); 
+    function validateForm() {
+        var nombre = document.getElementById("nombre").value;
+        var email = document.getElementById("email").value;
+        var telefono = document.getElementById("telefono").value;
+        var mensaje = document.getElementById("mensaje").value;
+        var telefonoPattern = /^\d{3}-\d{3}-\d{4}$/;
 
-   
-    const nombre = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const mensaje = document.getElementById('message').value;
+        var error = '';
 
-    
-    console.log(`Nombre: ${nombre}`);
-    console.log(`Correo Electrónico: ${email}`);
-    console.log(`Mensaje: ${mensaje}`);
+        if (!nombre || !email || !telefono || !mensaje) {
+            error += 'Todos los campos son obligatorios.<br>';
+        }
 
-  
-    alert('Mensaje enviado con éxito. Gracias por contactarnos.');
-});
-</script>
+        if (nombre.length > 50) {
+            error += 'El nombre no puede tener más de 50 caracteres.<br>';
+        }
+
+        if (email.length > 100 || !isValidEmail(email)) {
+            error += 'Por favor, ingresa un email válido.<br>';
+        }
+
+        if (telefono.length > 12 || !telefono.match(telefonoPattern)) {
+            error += 'Por favor, ingresa un número de teléfono válido en formato 123-456-7890.<br>';
+        }
+
+        if (mensaje.length > 500) {
+            error += 'El mensaje no puede tener más de 500 caracteres.<br>';
+        }
+
+        if (error) {
+            document.getElementById('result').innerHTML = '<div style="color:red">' + error + '</div>';
+            return false;
+        }
+
+        var resultHTML = '<div style="color:green">Datos enviados:<br>';
+        resultHTML += 'Nombre: ' + nombre + '<br>';
+        resultHTML += 'Email: ' + email + '<br>';
+        resultHTML += 'Teléfono: ' + telefono + '<br>';
+        resultHTML += 'Mensaje: ' + mensaje + '</div>';
+
+        document.getElementById('result').innerHTML = resultHTML;
+        return false;
+    }
+
+    function isValidEmail(email) {
+        var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        return emailPattern.test(email);
+    }

eac80b701cd2754d934a79698ca0614c8883aae5
Ashvax <137654244+Ashvax@users.noreply.github.com>
2023-10-17 15:34:30 -0300

    form

--- formulario.js
+++ formulario.js
@@ -1,56 +1,19 @@
+<script>
+// Función para manejar el envío del formulario
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault(); 
 
-function mostrarMensaje(mensaje) {
-    console.log(mensaje);
-  }
-  
+   
+    const nombre = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const mensaje = document.getElementById('message').value;
 
-  const form = document.createElement('form');
-  
-  const nameInput = document.createElement('input');
-  nameInput.type = 'text';
-  nameInput.placeholder = 'Nombre';
-  
-  const emailInput = document.createElement('input');
-  emailInput.type = 'email';
-  emailInput.placeholder = 'Correo Electrónico';
-  
- 
-  const messageInput = document.createElement('textarea');
-  messageInput.placeholder = 'Mensaje';
-  
-  
-  const submitButton = document.createElement('button');
-  submitButton.textContent = 'Enviar';
-  
-  
-  form.appendChild(nameInput);
-  form.appendChild(emailInput);
-  form.appendChild(messageInput);
-  form.appendChild(submitButton);
-  
-  
-  document.body.appendChild(form);
-  
-  // Función para manejar el envío del formulario
-  form.addEventListener('submit', function (event) {
-    event.preventDefault(); // Evitar que el formulario se envíe
-  
-  
-    const nombre = nameInput.value;
-    const email = emailInput.value;
-    const mensaje = messageInput.value;
-  
     
-    mostrarMensaje(`Nombre: ${nombre}`);
-    mostrarMensaje(`Correo Electrónico: ${email}`);
-    mostrarMensaje(`Mensaje: ${mensaje}`);
-  
- 
-    mostrarMensaje("Mensaje enviado con éxito. Gracias por contactarnos.");
-  
+    console.log(`Nombre: ${nombre}`);
+    console.log(`Correo Electrónico: ${email}`);
+    console.log(`Mensaje: ${mensaje}`);
 
-    nameInput.value = '';
-    emailInput.value = '';
-    messageInput.value = '';
-  });
   
+    alert('Mensaje enviado con éxito. Gracias por contactarnos.');
+});
+</script>

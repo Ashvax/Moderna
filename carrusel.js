439fbb17bd1d19777d4fe0260eb7bae74172a721
Ashvax <137654244+Ashvax@users.noreply.github.com>
2023-10-18 16:11:36 -0300

    carru

--- carrusel.js
+++ carrusel.js
@@ -1,55 +1,56 @@
-let botonAnt= document.getElementById('left')
-let botonSig= document.getElementById('right')
-let contenedorPuntos= document.getElementById('puntitosContainer')
+let botonAnt = document.getElementById('left')
+let botonSig = document.getElementById('right')
+let contenedorPuntos = document.getElementById('puntitosContainer')
 let imagen = document.getElementById('imagenView')
 
-let coleccion = ["./sources/b1.jpg","./sources/b2.jpg","./sources/b3.jpg","./sources/b4.jpg", "./sources/b5.jpg","./sources/b6.jpg","./sources/b7.jpg","./sources/b8.jpg","./sources/b9.png"]
+let coleccion = ["./sources/b1.jpg", "./sources/b2.jpg", "./sources/b3.jpg", "./sources/b4.jpg", "./sources/b5.jpg", "./sources/b6.jpg", "./sources/b7.jpg", "./sources/b8.jpg", "./sources/b9.png"]
 
-let puntero=0;
+let puntero = 0;
 
-window.addEventListener('load', ()=>{
-    imagen.src= coleccion[puntero]
+window.addEventListener('load', () => {
+    imagen.src = coleccion[puntero]
     cargarPuntitos()
     actualizarPuntos()
 })
 
-function cargarPuntitos(){
-    coleccion.forEach(()=>{
+function cargarPuntitos() {
+    coleccion.forEach((item, index) => {
         let p = document.createElement('div')
         p.classList.add('punto')
+        p.setAttribute('data-index', index) // Almacenamos el índice en el atributo data
         contenedorPuntos.appendChild(p)
+
+        // Agregar evento de clic a cada puntito
+        p.addEventListener('click', () => {
+            puntero = parseInt(p.getAttribute('data-index'), 10)
+            imagen.src = coleccion[puntero]
+            actualizarPuntos()
+        })
     })
 }
 
-
-botonSig.addEventListener('click',()=>{
+botonSig.addEventListener('click', () => {
     puntero++
-
-    if(puntero>=coleccion.length){
-        puntero=0
+    if (puntero >= coleccion.length) {
+        puntero = 0
     }
-    imagen.src= coleccion[puntero]
+    imagen.src = coleccion[puntero]
     actualizarPuntos()
-
 })
 
-botonAnt.addEventListener('click',()=>{
+botonAnt.addEventListener('click', () => {
     puntero--
-    if(puntero<0){
-        puntero=coleccion.length-1
+    if (puntero < 0) {
+        puntero = coleccion.length - 1
     }
-
-    imagen.src= coleccion[puntero]
+    imagen.src = coleccion[puntero]
     actualizarPuntos()
 })
 
-function actualizarPuntos(){
-    let pts= document.querySelectorAll('.punto')
-    
-    pts.forEach(p=>{
+function actualizarPuntos() {
+    let pts = document.querySelectorAll('.punto')
+    pts.forEach(p => {
         p.classList.remove('puntoActual')
     })
-
     pts[puntero].classList.add('puntoActual')
-
 }

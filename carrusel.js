dc7d45c03dbd8311f5cb2813f098b11d74a086be
Ashvax <137654244+Ashvax@users.noreply.github.com>
2023-10-18 16:32:56 -0300

    cambio automaticamente cada 5 sg

--- carrusel.js
+++ carrusel.js
@@ -6,11 +6,15 @@ let imagen = document.getElementById('imagenView')
 let coleccion = ["./sources/b1.jpg", "./sources/b2.jpg", "./sources/b3.jpg", "./sources/b4.jpg", "./sources/b5.jpg", "./sources/b6.jpg", "./sources/b7.jpg", "./sources/b8.jpg", "./sources/b9.png"]
 
 let puntero = 0;
+let intervalo;
 
 window.addEventListener('load', () => {
     imagen.src = coleccion[puntero]
     cargarPuntitos()
     actualizarPuntos()
+
+    
+    intervalo = setInterval(cambiarImagenAutomaticamente, 5000);
 })
 
 function cargarPuntitos() {
@@ -20,35 +24,42 @@ function cargarPuntitos() {
         p.setAttribute('data-index', index)
         contenedorPuntos.appendChild(p)
 
-       
+        
         p.addEventListener('click', () => {
             puntero = parseInt(p.getAttribute('data-index'), 10)
             imagen.src = coleccion[puntero]
             actualizarPuntos()
         })
 
-        
+       
         p.style.cursor = 'pointer';
     })
 }
 
 botonSig.addEventListener('click', () => {
-    puntero++
-    if (puntero >= coleccion.length) {
-        puntero = 0
-    }
-    imagen.src = coleccion[puntero]
-    actualizarPuntos()
+    cambiarImagen(1); 
 })
 
 botonAnt.addEventListener('click', () => {
-    puntero--
-    if (puntero < 0) {
-        puntero = coleccion.length - 1
-    }
-    imagen.src = coleccion[puntero]
-    actualizarPuntos()
+    cambiarImagen(-1); 
 })
+
+function cambiarImagenAutomaticamente() {
+    cambiarImagen(1); 
+}
+
+function cambiarImagen(direccion) {
+    puntero += direccion;
+
+    if (puntero >= coleccion.length) {
+        puntero = 0;
+    } else if (puntero < 0) {
+        puntero = coleccion.length - 1;
+    }
+
+    imagen.src = coleccion[puntero];
+    actualizarPuntos();
+}
 
 function actualizarPuntos() {
     let pts = document.querySelectorAll('.punto')
